3ec53983ae19116f1a5547ccd5e0802a2899ebba
DanEternity <Dan.spirov@yandex.ru>
2019-05-03 15:47:55 +0300

    network

--- clientCourse/MainForm.cpp
+++ clientCourse/MainForm.cpp
@@ -1,6 +1,23 @@
+#define WIN32_LEAN_AND_MEAN
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
 #include "MainForm.h"
 #include "SignIn.h"
 #include "SignUp.h"
+#include "Network.h"
+
+
+//#include <windows.h>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+
 
 using namespace clientCourse;
 
@@ -11,10 +28,20 @@ int main(array<System::String^> ^args)
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
+	SOCKET socket;
+	if (NetworkSetup(socket) != 0)
+	{
+		WSACleanup();
+		return 0; // error
+	}
+
 	//Создание окна авторизации
 	Application::Run(gcnew SignIn());
 
 	// Создание главного окна и его запуск
 	Application::Run(gcnew MainForm());
+
+
+	WSACleanup();
 	return 0;
 }
---
+++ clientCourse/Network.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+#define DEFAULT_BUFLEN 512
+
+int NetworkSetup(SOCKET & client_socket)
+{
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+	char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
+	int iResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	char inet_addr[50] = "";
+	int port = 27015;
+
+	std::ifstream fin("config.txt");
+	if (!fin.is_open())
+	{
+		return 1; // config not found;
+	}
+
+	while (!fin.eof() && !fin.fail())
+	{
+		std::string st;
+		fin >> st;
+
+		if (st == "#default_ip")
+			fin >> inet_addr;
+		
+		if (st == "#default_port")
+			fin >> port;
+	}
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+
+	char serv_ip[50] = "", serv_port[50] = "";
+	memcpy(&serv_ip, &inet_addr, 48U);
+
+	std::string t_port = std::to_string(port);
+	memcpy(&serv_port, t_port.c_str(), t_port.size());
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(serv_ip, serv_port, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+	printf("Connecting...\n");
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			return 1;
+		}
+
+
+
+		// Connect to server.
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+
+	client_socket = ConnectSocket;
+}
